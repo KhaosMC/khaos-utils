d156ae992fe45c4c064cb6503f65a297f6209efd
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-09-22 08:53:21 +0200

    Dog api support for animal command + switch case

--- commands/animal.js
+++ commands/animal.js
@@ -3,23 +3,34 @@ const fetch = require('node-fetch');
 
 module.exports = {
     description: 'Fetch image of an animal',
-    usage: '(cat)',
+    usage: '(cat/dog)',
     commandGroup: 'misc',
     requiredRole: null,
     guildOnly: false,
     requireManageGuild: false,
     guildOwnerOnly: false,
     run: async (client, message, args, commands, config) => {
-        if (args[0] === 'cat') {
-            const catRequest = await fetch(`https://api.thecatapi.com/v1/images/search?mime_types=gif`, {
-                headers: { 'x-api-key' : config.catApiToken }
-            }).then(response => response.json());
-            const embed = new MessageEmbed()
-            .setTitle('Cat payload!')
-            .setImage(catRequest[0].url)
-            .setFooter(message.author.tag, message.author.avatarURL())
+        switch (args[0]) {
+            case 'cat':
+                const catRequest = await fetch(`https://api.thecatapi.com/v1/images/search?mime_types=gif`, {
+                    headers: { 'x-api-key' : config.catApiToken }
+                }).then(response => response.json());
+                const catEmbed = new MessageEmbed()
+                .setTitle('Cat payload!')
+                .setImage(catRequest[0].url)
+                .setFooter(message.author.tag, message.author.avatarURL())
+    
+                message.channel.send(catEmbed)
+                break;
+            case 'dog':
+                const dogRequest = await fetch(`https://dog.ceo/api/breeds/image/random`).then(response => response.json());
+                const dogEmbed = new MessageEmbed()
+                .setTitle('Dog payload!')
+                .setImage(dogRequest.message)
+                .setFooter(message.author.tag, message.author.avatarURL())
 
-            message.channel.send(embed)
+                message.channel.send(dogEmbed)
+                break;
         }
     }
 }
