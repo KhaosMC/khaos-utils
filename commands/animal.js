6997df14589a136358bf737c083adf7f1d7bd6a1
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-09-22 14:01:51 +0200

    Change api to flickr only & refactor command
    This command only uses Flickr API to fetch images, then parses them and displays them back to the user. I check against an array to easily be able to display the supported animals.

--- commands/animal.js
+++ commands/animal.js
@@ -3,47 +3,71 @@ const fetch = require('node-fetch');
 
 module.exports = {
     description: 'Fetch image of an animal',
-    usage: '(cat/dog/axolotl/fox)',
+    usage: '(cat/dog/axolotl/fox/monkey)',
     commandGroup: 'misc',
     requiredRole: null,
     guildOnly: false,
     requireManageGuild: false,
     guildOwnerOnly: false,
     run: async (client, message, args, commands, config) => {
-        function getEmbed(type, media) {
+        let supportedAnimals = [
+            'cat',
+            'dog',
+            'axolotl',
+            'fox',
+            'monkey'
+        ]
+        async function fetchMedia(type) {
+            const request = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=85d5d441375458df3dbe2bc67bdff8d9&tags=${type}&format=json&nojsoncallback=1`)
+            .then(response => response.json());
+            const rng = Math.floor(Math.random() * 100);
+            const image = request.photos.photo[rng]
+            const url = `https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
+
             const embed = new MessageEmbed()
             .setTitle(`${type} payload!`)
-            .setImage(media)
-            .setFooter(message.author.tag, message.author.avatarURL())
+            .setImage(url)
+            .setFooter("Image provied by Flickr")
 
-            return embed
+            return embed;
         }
-
         switch (args[0]) {
-            case 'cat': {
-                const catRequest = await fetch(`https://api.thecatapi.com/v1/images/search?mime_types=gif`, {
-                    headers: { 'x-api-key' : config.catApiToken }
-                }).then(response => response.json());
-                message.channel.send(getEmbed('cat', catRequest[0].url))
+            case supportedAnimals[0]: {
+                const embed = await fetchMedia(supportedAnimals[0]);
+                message.channel.send(embed)
                 break;
             }
-            case 'dog': {
-                const dogRequest = await fetch(`https://dog.ceo/api/breeds/image/random`).then(response => response.json());
-                message.channel.send(getEmbed('dog', dogRequest.message))
+            case supportedAnimals[1]: {
+                const embed = await fetchMedia(supportedAnimals[1]);
+                message.channel.send(embed)
                 break;
             }
-            case 'axolotl': {
-                const axolotlRequest = await fetch('https://axoltlapi.herokuapp.com/').then(response => response.json());
-                message.channel.send(getEmbed('axolotl', axolotlRequest.url))
+            case supportedAnimals[2]: {
+                const embed = await fetchMedia(supportedAnimals[2]);
+                message.channel.send(embed)
                 break;
             }
-            case 'fox': {
-                const foxRequest = await fetch('https://randomfox.ca/floof/').then(response => response.json())
-                message.channel.send(getEmbed('fox', foxRequest.image))
+            case supportedAnimals[3]: {
+                const embed = await fetchMedia(supportedAnimals[3]);
+                message.channel.send(embed)
+                break;
+            }
+            case supportedAnimals[4]: {
+                const embed = await fetchMedia(supportedAnimals[4]);
+                message.channel.send(embed)
+                break;
+            }
+            case 'list': {
+                const listOfSupported = new MessageEmbed()
+                .setTitle("Supported animals!")
+                .setDescription(supportedAnimals.join(', '))
+                .setFooter(message.author.tag, message.author.avatarURL())
+
+                message.channel.send(listOfSupported)
                 break;
             }
             default: {
-                const msg = await message.channel.send("Invalid animal! See `" + config.prefix + "help misc`.")
+                const msg = await message.channel.send("Invalid animal! See `" + config.prefix + "animal list`.")
                 setTimeout(() => {
                     msg.delete().catch();
                 }, 5000);
