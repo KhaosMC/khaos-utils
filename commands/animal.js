33d441ed2284cb1b0eb30add434d91883d2a0487
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-09-22 08:59:12 +0200

    Axolotl support for animal command

--- commands/animal.js
+++ commands/animal.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 module.exports = {
     description: 'Fetch image of an animal',
-    usage: '(cat/dog)',
+    usage: '(cat/dog/axolotl)',
     commandGroup: 'misc',
     requiredRole: null,
     guildOnly: false,
@@ -30,6 +30,15 @@ module.exports = {
                 .setFooter(message.author.tag, message.author.avatarURL())
 
                 message.channel.send(dogEmbed)
+                break;
+            case 'axolotl':
+                const axolotlRequest = await fetch ('https://axoltlapi.herokuapp.com/').then(response => response.json());
+                const axolotlEmbed = new MessageEmbed()
+                .setTitle('Axolotl payload!')
+                .setImage(axolotlRequest.url)
+                .setFooter(message.author.tag, message.author.avatarURL())
+
+                message.channel.send(axolotlEmbed)
                 break;
         }
     }
