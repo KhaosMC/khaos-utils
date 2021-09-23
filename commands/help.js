7c873f6811f598c0e66580e86f55a8b8b3e693c2
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-09-23 11:08:20 +0200

    Fix help command

--- commands/help.js
+++ commands/help.js
@@ -13,15 +13,16 @@ module.exports = {
         let allCommands = [];
         let title;
         let footer;
-            if (args[0] !== null) {
+            if (args[0] !== undefined) {
                 title = `Commands in group ${args[0]}!`
                 footer = '[] = required, () = optional'
                 commands.forEach((value, commandName, commands) => {
                     const command = commands.get(commandName);
 
-                    if ((command.requireRole === null || message.member.roles.cache.get(requiredRole)) && args[0] == command.commandGroup) {
+                    if ((command.requiredRole === null || message.member.roles.cache.get(command.requiredRole)) && args[0] === command.commandGroup) {
+                        console.log(commandName)
                         if (command.requireManageGuild && !(message.member.hasPermission('MANAGE_GUILD'))) {
-                            
+
                         } else if (command.guildOwnerOnly && !(message.author === message.guild.owner)) {
                             
                         } else {
