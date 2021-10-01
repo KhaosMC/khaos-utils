29026915b613ff918f65df805d1325e875f568a0
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-10-01 13:58:25 +0200

    randomize color for serverinfo

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,16 +9,18 @@ module.exports = {
     requiredPermission: null,
     guildOwnerOnly: false,
     run: async (client, message, args, commands, config) => {
+        const color = parseInt(Math.floor(Math.random() * (Math.pow(2, 24)) - 1), 10);
+
         const embed = new MessageEmbed()
         .setTitle(message.guild.name)
         .setThumbnail(message.guild.iconURL())
+        .setColor(color)
         .addField("Creation date", message.guild.createdAt, true)
         .addField("Owner", message.guild.owner.user.tag, true)
         .addField("Members", message.guild.memberCount, true)
         .addField("Emojis", message.guild.emojis.cache.size, true)
         .addField("Roles", message.guild.roles.cache.size, true)
         .setFooter(message.author.tag, message.author.avatarURL());
-
 
         message.channel.send(embed)
     }
