7679448901bdb2412b04ee60957f7a1767c41419
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-10-01 14:32:03 +0200

    Prevent `@everyone` mentions for chatbridge

--- websocket/chat_message.js
+++ websocket/chat_message.js
@@ -3,11 +3,11 @@ module.exports = {
     run: async (data, chatbridge, client, config) => {
         // Parse message to prevent discord markdown
         const discordMD = ['`', '*', '_', '~~', '||'];
-        let message = data.payload.message;
+        var message = data.payload.message;
         discordMD.forEach(char => {
             message = message.replace(char, `\\${char}`)
         });
 
-        client.channels.cache.get(chatbridge.channel_id).send(`[${data.source.name}] <${data.payload.user.name}> ${message}`);
+        client.channels.cache.get(chatbridge.channel_id).send(`[${data.source.name}] <${data.payload.user.name}> ${message}`, {disableMentions: "everyone"});
     }
 }
