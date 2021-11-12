117c2b2e96d32c4df8a678d0ff620d740bd6cdf8
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-11-12 21:05:45 +0100

    purge command

--- commands/purge.js
+++ commands/purge.js
@@ -8,20 +8,26 @@ module.exports = {
     guildOnly: false,
     requiredPermission: 'MANAGE_MESSAGES',
     guildOwnerOnly: false,
-    run: async (client, message, args, commands, config) => {
+    run: async (bot, message, args) => {
         const amount = parseInt(args[0]) + 1;
-        if (!amount || amount == 0 || amount > 10000) return message.channel.send("Invalid amount!").then(msg => msg.delete({timeout: 5000}));
+        if (!amount || amount == 0 || amount > 10000) return message.channel.send("Invalid amount!").then(msg => {
+            setTimeout(() => msg.delete().catch(), 5000)
+        });
         // Setup the amount of times you can delete 100
         const ranges = Math.floor(amount / 100);
         // Delete for every 100 messages
         for (i = 0; i < ranges; i++) {
             message.channel.bulkDelete(i * 100)
-            .catch(err => message.channel.send(`${err} occured`)).then(msg => msg.delete({timeout:5000}));
+            .catch(err => message.channel.send(`${err} occured`)).then(msg => {
+                setTimeout(() => msg.delete(), 5000);
+            });
             setTimeout(() => {}, 5000); 
         }
         // Delete if there is a left over amount that is not divisible by 100
         if (amount % 100 !== 0) message.channel.bulkDelete(amount % 100)
-        .catch(err => message.channel.send(`${err} occured`)).then(msg => msg.delete({timeout:5000}));
+        .catch(err => message.channel.send(`${err} occured`)).then(msg => {
+            setTimeout(() => msg.delete(), 5000);
+        });
         // Send embed and then delete the message
         const embed = new MessageEmbed()
         .setTitle(`Delete ${amount - 1} messages.`)
@@ -30,6 +36,8 @@ module.exports = {
         .setDescription(`Requested by ${message.author.tag}`)
         .setTimestamp();
         
-        message.channel.send(embed).then(msg => msg.delete({timeout:5000}));
+        message.channel.send({embeds: [embed]}).then(msg => {
+            setTimeout(() => msg.delete(), 5000);
+        });
     }
 }
