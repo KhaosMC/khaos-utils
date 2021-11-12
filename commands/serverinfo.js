5f5ddd5ff22b08ce32b73a7472473a024cbf310e
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-11-12 19:44:55 +0100

    serverinfo command

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require('discord.js');
-const { memberRole, inactiveRole } = require ('../config/config.json')
 
 module.exports = {
     description: 'Get info about server',
@@ -9,27 +8,27 @@ module.exports = {
     guildOnly: true,
     requiredPermission: null,
     guildOwnerOnly: false,
-    run: async (client, message, args, commands, config) => {
+    run: async (bot, message, args) => {
         // Randomize a color by randomizing between 0-2^24-1
         const color = Math.floor(Math.random() * (Math.pow(2, 24)) - 1);
         const age = Math.floor((Date.now() - message.guild.createdTimestamp) / (1000*60*60*24));
-        const allMembers = message.guild.roles.cache.get(memberRole).members.size;
-        const activeMembers = allMembers - message.guild.roles.cache.get(inactiveRole).members.size;
+        const allMembers = message.guild.roles.cache.get(bot.config.memberRole).members.size;
+        const activeMembers = allMembers - message.guild.roles.cache.get(bot.config.inactiveRole).members.size;
+        const owner = await message.guild.fetchOwner();
 
         const embed = new MessageEmbed()
         .setTitle(message.guild.name)
         .setThumbnail(message.guild.iconURL())
         .setColor(color)
-        .addField("Creation date", message.guild.createdAt, true)
+        .addField("Creation date", message.guild.createdAt.toString(), true)
         .addField("Age", `${age} days old`)
-        .addField("Owner", message.guild.owner.user.tag, true)
+        .addField("Owner", owner.user.tag, true)
         .addField("Members", `${message.guild.memberCount}/${message.guild.maximumMembers}`, true)
         .addField("Minecraft Members", `${activeMembers}/${allMembers} active`, true)
-        .addField("Emojis", message.guild.emojis.cache.size, true)
-        .addField("Roles", message.guild.roles.cache.size, true)
-        .addField("Region", message.guild.region, true)
+        .addField("Emojis", message.guild.emojis.cache.size.toString(), true)
+        .addField("Roles", message.guild.roles.cache.size.toString(), true)
         .setFooter(message.author.tag, message.author.avatarURL());
 
-        message.channel.send(embed)
+        message.channel.send({embeds: [embed]});
     }
 }
