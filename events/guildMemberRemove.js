6dee4e840d4de98b2d12bbd67a2de769243468f2
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-11-12 21:52:43 +0100

    Update events

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,6 @@
 module.exports = {
     description: 'guildMemberRemove event',
-    run: async (client, config, socket, db, member) => {
+    run: async (bot, member) => {
         // Timeout for 300ms incase things weren't updated properly
         setTimeout(() => {}, 1000);
         // Check if there's a welcome channel
@@ -11,18 +11,18 @@ module.exports = {
         const bansArray = Array.from(bans.keys());
         if (bansArray.includes(member.user.id)) return;
         // Check if the user was kicked
-        /* const auditLog = await member.guild.fetchAuditLogs({
+        const auditLog = await member.guild.fetchAuditLogs({
             limit: 1,
             type: 'MEMBER_KICK',
         });
-        let targetId = null
+        let targetId = 0
         let auditLogTimestamp = 0
         if (auditLog.entries.size !== 0) { 
-            targetId = auditLog.entries[0].target.id.catch();
-            auditLogTimestamp = auditLog.entries[0].createdTimestamp.catch();
+            targetId = auditLog.entries.first().target.id.catch();
+            auditLogTimestamp = auditLog.entries.first().createdTimestamp.catch();
         }
         if (targetId === member.user.id.catch() && (Date.now() - auditLogTimestamp) < 6000) return console.log(member.user.tag);
-        */
+
         // If not, send the goodbye message
         welcomeChannel.send(`o/n't ${member.user.tag}`);
     }
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -5,14 +5,14 @@ const commandsConfig = JSON.parse(fs.readFileSync('./config/commands.json'));
 
 module.exports = {
     description: 'messageReactionAdd event',
-    run: async (client, config, socket, db, messageReaction, user) => {
+    run: async (bot, messageReaction, user) => {
         // Check if it's the app message, if apps are enabled and the user isn't a bot
-        if (messageReaction.message.id === config.applicationMessage[1] && commandsConfig.applications && !user.bot) {
+        if (messageReaction.message.id === bot.config.applicationMessage[1] && bot.commandsConfig.applications && !user.bot) {
             // Resolve the user and check if they have an app
             const member = messageReaction.message.guild.members.resolve(user);
             const hasApp = await db.get('SELECT 1 FROM application_channels WHERE user_id = ? AND open LIMIT 1;', user.id);
             // If they're not a member and don't have an app, proceed
-            if (!hasApp && !(member.roles.cache.get(config.memberRole))) {
+            if (!hasApp && !(member.roles.cache.get(bot.config.memberRole))) {
                 // Create channel with the applicant's username, then let them + members read it.
                 const appChannel = await messageReaction.message.guild.channels.create(user.username, {
                     type: "text",
@@ -40,7 +40,7 @@ module.exports = {
                 .setDescription("Please make sure you have read the rules in the channel. We will shortly get in touch with you to discuss your application and send you the form.")
                 .setFooter('Best of luck!');
 
-                appChannel.send(newApp);
+                appChannel.send({embeds: [newApp]});
             }
             // Delete all reactions
             messageReaction.users.remove(user.id).catch();
