d9862d9c26c4af26ecc95bda7de54ff5041c1fb5
Maheesha <maheeshasithum@gmail.com>
2021-11-13 23:45:12 +0530

    Pushing so ains can debug

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,38 +1,41 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Permissions} = require('discord.js');
 const fs = require('fs');
+const Console = require("console");
 const config = JSON.parse(fs.readFileSync('./config/config.json'));
 const commandsConfig = JSON.parse(fs.readFileSync('./config/commands.json'));
 
 module.exports = {
     description: 'messageReactionAdd event',
     run: async (bot, messageReaction, user) => {
+        console.log("hiiiiii");
         // Check if it's the app message, if apps are enabled and the user isn't a bot
-        if (messageReaction.message.id === bot.config.applicationMessage[1] && bot.commandsConfig.applications && !user.bot) {
+        if (messageReaction.message.id === bot.config.applicationMessage[1] /*&& bot.commandsConfig.applications*/ && !user.bot) {
             // Resolve the user and check if they have an app
             const member = messageReaction.message.guild.members.resolve(user);
-            const hasApp = await db.get('SELECT 1 FROM application_channels WHERE user_id = ? AND open LIMIT 1;', user.id);
+            const hasApp = await bot.db.get('SELECT 1 FROM application_channels WHERE user_id = ? AND open LIMIT 1;', user.id);
             // If they're not a member and don't have an app, proceed
-            if (!hasApp && !(member.roles.cache.get(bot.config.memberRole))) {
+            if (/*!hasApp &&*/ !(member.roles.cache.get(bot.config.memberRole))) {
+                console.log("hi");
                 // Create channel with the applicant's username, then let them + members read it.
-                const appChannel = await messageReaction.message.guild.channels.create(user.username, {
-                    type: "text",
+                const appChannel = await messageReaction.message.guild.channels.create('bruh', {
+                    type: 'GUILD_TEXT',
                     permissionOverwrites: [
                         {
                             id: messageReaction.message.guild.roles.everyone,
-                            deny: ['VIEW_CHANNEL']
+                            deny: [Permissions.FLAGS.VIEW_CHANNEL]
                         },
                         {
-                            id: config.memberRole,
-                            allow: ['VIEW_CHANNEL']
+                            id: bot.config.memberRole,
+                            allow: [Permissions.FLAGS.VIEW_CHANNEL]
                         },
                         {
                             id: user.id,
-                            allow: ['VIEW_CHANNEL']
+                            allow: [Permissions.FLAGS.VIEW_CHANNEL]
                         }
                     ]
                 });
                 // Add to database, create embed and send in app channel
-                await db.run(`INSERT INTO application_channels VALUES (?, ?, NULL, 1);`, appChannel.id, user.id);
+                await bot.db.run(`INSERT INTO application_channels VALUES (?, ?, NULL, 1);`, appChannel.id, user.id);
                 const newApp = new MessageEmbed()
                 .setTitle('Hello applicant!')
                 .setThumbnail(user.avatarURL())
