370317cb140525062a00cd32b2bafdb0df9b93ca
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-11-13 19:57:47 +0100

    Make reason a variable so we can reassign it

--- commands/ban.js
+++ commands/ban.js
@@ -11,7 +11,7 @@ module.exports = {
     run: async (bot, message, args) => {
         // Check permission and if person specified a user
         const toKick = message.mentions.members.first() || bot.client.users.cache.get(args[0]);
-        const reason = args.slice(1).join(" ");
+        let reason = args.slice(1).join(" ");
         if (!reason) reason = 'Unknown';
         const member = message.guild.members.resolve(toKick);
         if(!member) return message.channel.send("You need to specify a user!").then(msg => setTimeout(() => msg.delete()),bot.config.deleteTimer);
