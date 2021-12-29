d7f227951038a7e108da1cca0729229542e2b03b
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-12-29 13:04:48 +0100

    make deepsource stop complain about my shitty code

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
-const logger = require('./modules/logger');
 const init = require('./modules/init.js');
 const validate = require('./modules/validate.js');
-const utils = require('./modules/utils.js');
 
 async function initialize() {
 	var bot = {
 		fs: fs,
 		dotenv: await init.env(),
 		client: await init.discord(),
-		logger: logger,
-		utils: utils,
+		logger: require('./modules/logger.js'),
+		utils: require('./modules/utils.js'),
 		config: await JSON.parse(fs.readFileSync('./config/config.json')),
                 commandsConfig: await JSON.parse(fs.readFileSync('./config/commands.json')),
                 tags: await JSON.parse(fs.readFileSync('./config/tags.json')),
